da6d6a5b3b290d73af66571bba478143cd85b636
Mateusz Chrzonstowski <mateusz.chrzonstowski@paramount.com>
2023-12-27 16:46:45 +0100

    Simplify 22, 1

--- 22-1.js
+++ 22-1.js
@@ -1,28 +1,20 @@
 export function countRemovals(bricks) {
-    const adjacency = findAdjacent(bricks);
-    const groups = groupByZ(bricks);
+    const [adjacency, reverseAdjacency] = findAdjacent(bricks);
     let result = 0;
-    [...groups.values()].forEach((indicesSet) => {
-        indicesSet.forEach((index) => {
-            const colliding = adjacency.get(index);
-            if (colliding.size === 0) {
-                result++;
-                console.info(toString(bricks[index]))
-                return;
-            }
-            const allButCurrentCollisions = [...indicesSet].filter(i => i !== index).map(i => adjacency.get(i));
-            // can be removed if all colliding are covered by other bricks
-            if ([...colliding].every(i => allButCurrentCollisions.some(collisions => collisions.has(i)))) {
-                result++;
-                console.info(toString(bricks[index]))
-            }
-        });
+    bricks.forEach((brick, index) => {
+        const colliding = adjacency.get(index);
+        // can be removed if all colliding have alternative bricks to rest on
+        if ([...colliding].every(i => reverseAdjacency.get(i).size !== 1)) {
+            console.info(toString(brick))
+            result++;
+        }
     });
     return result;
 }
 
 function findAdjacent(bricks) {
     const adjacency = new Map();
+    const reverseAdjacency = new Map();
     for (let i = 0; i < bricks.length; i++) {
         adjacency.set(i, new Set());
         const currentBrick = bricks[i];
@@ -31,25 +23,14 @@ function findAdjacent(bricks) {
             const {result} = collides(currentBrick, potentialCollision);
             if (result && currentBrick.z + currentBrick.zLength === z) {
                 adjacency.get(i).add(j);
+                if (!reverseAdjacency.has(j)) {
+                    reverseAdjacency.set(j, new Set());
+                }
+                reverseAdjacency.get(j).add(i);
             }
         }
     }
-    return adjacency;
-}
-
-function groupByZ(bricks) {
-    const groups = new Map();
-    let recentZ = bricks[0].z;
-    groups.set(recentZ, new Set([0]));
-    for (let i = 1; i < bricks.length; i++) {
-        if (bricks[i].z === recentZ) {
-            groups.get(recentZ).add(i);
-            continue;
-        }
-        recentZ = bricks[i].z;
-        groups.set(recentZ, new Set([i]));
-    }
-    return groups;
+    return [adjacency, reverseAdjacency];
 }
 
 export function settle(input) {
